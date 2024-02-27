94ac204d957f581bd12605ccebc718dd2ba8bc6d
David Gonzalez Marchante <Davidgonmar91@gmail.com>
2024-02-27 18:24:41 +0100

    PrimerCommitTileset
    Afegits el tileset en format png i el tsx per el Tiled

---
+++ Tiled/Pang.tsx
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Pang" tilewidth="8" tileheight="8" tilecount="100" columns="10" fillmode="preserve-aspect-fit">
+ <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
+ <image source="pangtileset.png" trans="898989" width="80" height="80"/>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="1" y="0" width="7" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0217969" width="8" height="7.97767"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="7" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="0"/>
+ <tile id="1"/>
+ <tile id="2"/>
+ <tile id="43"/>
+ <tile id="3"/>
+ <tile id="63"/>
+ <tile id="4"/>
+ <tile id="5"/>
+ <tile id="6"/>
+ <tile id="65"/>
+ <tile id="22"/>
+ <tile id="53"/>
+ <tile id="23"/>
+ <tile id="54"/>
+ <tile id="26"/>
+ <tile id="28"/>
+ <tile id="29"/>
+ <tile id="30"/>
+ <tile id="31"/>
+ <tile id="32"/>
+ <tile id="33"/>
+ <tile id="34"/>
+ <tile id="35"/>
+ <tile id="44"/>
+ <tile id="7"/>
+ <tile id="8"/>
+ <tile id="9"/>
+ <tile id="10"/>
+ <tile id="64"/>
+ <tile id="11"/>
+ <tile id="24"/>
+ <tile id="12"/>
+ <tile id="13"/>
+ <tile id="25"/>
+ <tile id="14"/>
+ <tile id="38"/>
+ <tile id="39"/>
+ <tile id="40"/>
+ <tile id="41"/>
+ <tile id="42"/>
+ <tile id="18"/>
+ <tile id="19"/>
+ <tile id="20"/>
+ <tile id="21"/>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="66"/>
+ <tile id="36"/>
+ <tile id="48"/>
+ <tile id="50"/>
+ <tile id="49"/>
+ <tile id="37"/>
+ <tile id="59"/>
+ <tile id="60"/>
+ <tile id="61"/>
+ <tile id="62"/>
+ <tile id="45"/>
+ <tile id="51"/>
+ <tile id="52"/>
+ <tile id="46"/>
+ <tile id="47"/>
+ <tile id="55"/>
+ <tile id="56"/>
+ <tile id="57"/>
+ <tile id="58"/>
+ <tile id="67"/>
+ <tile id="68"/>
+ <tile id="69"/>
+ <tile id="70"/>
+ <tile id="71"/>
+ <tile id="72"/>
+ <tile id="73"/>
+ <tile id="74"/>
+ <tile id="75"/>
+ <tile id="76"/>
+ <tile id="77"/>
+ <tile id="78"/>
+ <tile id="79"/>
+ <tile id="80"/>
+ <tile id="81"/>
+ <tile id="82"/>
+ <tile id="83"/>
+ <tile id="84"/>
+ <tile id="85"/>
+ <tile id="86"/>
+ <tile id="87"/>
+ <tile id="88"/>
+ <tile id="89"/>
+ <tile id="90"/>
+ <tile id="91"/>
+ <tile id="92"/>
+ <tile id="93"/>
+ <tile id="94"/>
+ <tile id="95"/>
+ <tile id="96"/>
+ <tile id="97"/>
+ <tile id="98"/>
+ <tile id="99"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="Escala" color="#aa7801" tile="-1" probability="1"/>
+   <wangcolor name="BlauDesSR" color="#000077" tile="-1" probability="1"/>
+   <wangcolor name="BlauDesAR" color="#7799dd" tile="-1" probability="1"/>
+   <wangcolor name="Mao" color="#0188ff" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,0,4,0,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,0,4,0"/>
+   <wangtile tileid="3" wangid="0,0,2,0,0,0,0,0"/>
+   <wangtile tileid="4" wangid="0,0,3,0,0,0,2,0"/>
+   <wangtile tileid="5" wangid="0,0,3,0,0,0,3,0"/>
+   <wangtile tileid="6" wangid="0,0,0,0,0,0,3,0"/>
+   <wangtile tileid="15" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="16" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="17" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="63" wangid="0,0,3,0,0,0,0,0"/>
+   <wangtile tileid="65" wangid="0,0,0,0,0,0,3,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
